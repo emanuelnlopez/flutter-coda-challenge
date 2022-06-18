7c668f91d43ee3fe5b3ff7a874be2b0261905746
Emanuel López <emanuel.n.lopez@gmail.com>
2022-06-18 09:22:32 -0300

    Refactors Validator utility class

--- app/lib/src/util/validator.dart
+++ app/lib/src/util/validator.dart
@@ -2,14 +2,20 @@ class Validator {
   static const _validEmailRegExp =
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
+  static const invalidEmailError = 'Invalid email';
+  static const passwordLengthError =
+      'Password should be at least 8 characters long';
+  static const passwordMismatchError = 'Passwords do not match';
+  static const requiredFieldError = 'Required field';
+
   static String? validateEmail(String? email) {
     String? result;
     if (email?.isNotEmpty != true) {
-      result = 'Required value';
+      result = requiredFieldError;
     }
     if (email?.isNotEmpty == true &&
         !RegExp(_validEmailRegExp).hasMatch(email!)) {
-      result = 'Invalid email';
+      result = invalidEmailError;
     }
     return result;
   }
@@ -17,10 +23,10 @@ class Validator {
   static String? validatePassword(String? password) {
     String? result;
     if (password?.isNotEmpty != true) {
-      result = 'Required value';
+      result = requiredFieldError;
     }
     if (password?.isNotEmpty == true && password!.length < 8) {
-      result = 'Password should be at least 8 characters long';
+      result = passwordLengthError;
     }
     return result;
   }
@@ -31,15 +37,15 @@ class Validator {
   }) {
     String? result;
     if (repeatPassword?.isNotEmpty != true) {
-      result = 'Required value';
+      result = requiredFieldError;
     }
     if (repeatPassword?.isNotEmpty == true && repeatPassword!.length < 8) {
-      result = 'Password should be at least 8 characters long';
+      result = passwordLengthError;
     }
     if (repeatPassword?.isNotEmpty == true &&
         repeatPassword!.length >= 8 &&
         repeatPassword != password) {
-      result = 'Passwords do not match';
+      result = passwordMismatchError;
     }
     return result;
   }
